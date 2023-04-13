bf979ac74e6db50607619abcc201a5d46d36ecc6
NongPhiHung20194581 <hung.np194581@sis.hust.edi.vn>
2023-04-13 19:42:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Product from "./components/Product";
+import Special from "./components/Special";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main className="main">
+        <Hero />
+        <Special />
+        <Product />
+      </main>
+    </>
+  );
+}
+
+export default App;
